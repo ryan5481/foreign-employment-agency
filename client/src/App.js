7abf0aef1b0ba27c7239cc0f734a178d3d200bec
RY4N <ry4n@192.168.0.101>
2023-09-13 21:11:06 +0800

    updated gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { useSelector } from "react-redux";
 import ConditionalRoute from "./routes/conditionalRoute"
 import AdminLogin from './containers/admin/auth/login';
 
